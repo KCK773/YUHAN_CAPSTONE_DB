e6654f22262301fcccc0a24cea6e057e66d3a7d1
KCK773 <alzhxh83@gmail.com>
2021-09-15 14:53:54 +0900

    add report file

---
+++ test_cap_09-08(오류)/test_cap/Program.cs
@@ -0,0 +1,34 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using Oracle.EntityFrameworkCore;
+using System;
+using System.Data.Odbc;
+using System.Data.OracleClient;
+
+namespace test_cap
+{
+    class DB_C
+    {
+        OracleConnection con;
+
+        void Connect()
+        {
+            con = new OracleConnection();
+            con.ConnectionString = "User Id=capid;Password=cappw;Data Source=onj";
+            con.Open();
+            Console.WriteLine("오라클 접속 OK");
+        }
+
+        void Close()
+        {
+            con.Close();
+            con.Dispose();
+        }
+
+        static void Main()
+        {
+            DB_C example = new DB_C();
+            example.Connect();
+            example.Close();
+        }
+    }
+}
